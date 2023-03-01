8f040e80a6577af2fdf4b3e6bdb702950c950980
SaraCarrerasC <sara.carreras@cloudmanlabs.com>
2023-03-01 10:00:55 +0100

    5th challenge

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Sock',
             image:'./assets/images/socks_blue.jpg',
             url: 'https://www.vuemastery.com',
@@ -9,9 +10,21 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        substractFromCart(){
+            this.cart -= 1
+        },
+        updateImage(vaiantImage){
+    this.image = vaiantImage
+        }
+
     }
 })
