ad9468619d9ed3027b08efa96ab40668c4a46d1c
SaraCarrerasC <sara.carreras@cloudmanlabs.com>
2023-03-01 13:05:54 +0100

    7th challenge

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Sock',
-            image:'./assets/images/socks_blue.jpg',
+            brand: 'Vue Mastery',
+            selectedVariant:0,
             url: 'https://www.vuemastery.com',
-            inStock: false,
             inventory:0,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' , quantity: 50},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',  quantity: 0 },
             ]
         }
     },
@@ -22,9 +22,24 @@ const app = Vue.createApp({
         substractFromCart(){
             this.cart -= 1
         },
-        updateImage(vaiantImage){
-    this.image = vaiantImage
+        updateVariant(index){
+    this.selectedVariant= index
+            console.log(index)
         }
 
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            return this.brand +" "+ this.product
+        }
     }
 })
