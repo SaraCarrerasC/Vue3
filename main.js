a58817527a2383ddd85befa6ce7815d353a66d50
SaraCarrerasC <sara.carreras@cloudmanlabs.com>
2023-03-01 09:15:33 +0100

    third challenge

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = Vue.createApp({
         return {
             product: 'Sock',
             image:'./assets/images/socks_blue.jpg',
-            url: 'https://www.vuemastery.com'
+            url: 'https://www.vuemastery.com',
+            inStock: true,
+            inventory:0,
+            onSale: true
         }
     }
 })
